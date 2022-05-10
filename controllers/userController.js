7559094e78043914c4e6b958f1faaddab3953ecc
Junshiun <55488934+Junshiun@users.noreply.github.com>
2022-05-10 17:24:17 +0800

    add user edit function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,4 +1,5 @@
 const user = require("../models/userModel");
+const notes = require("../models/notesModel");
 const asyncHandler = require("express-async-handler");
 const generateToken = require("../utils/generateJWT");
 
@@ -47,4 +48,63 @@ const authUser = asyncHandler(async (req, res) => {
   }
 });
 
-module.exports = { registerUser, authUser };
+const editUser = asyncHandler(async (req, res) => {
+  const { password, name, email, changePwd, newPassword } = req.body;
+
+  const currentUser = await user.findOne({ email });
+
+  if (currentUser) {
+    if (await currentUser.comparePwd(password)) {
+      if (changePwd === true) currentUser.password = newPassword;
+      currentUser.name = name;
+
+      const save = await currentUser.save();
+
+      res.status(201).json({
+        _id: currentUser._id,
+        name: currentUser.name,
+        email: currentUser.email,
+        isAdmin: currentUser.isAdmin,
+        token: generateToken(currentUser._id),
+      });
+    } else {
+      res.status(400);
+      throw new Error("invalid password");
+    }
+  } else {
+    res.status(400);
+    throw new Error("user not found");
+  }
+});
+
+const deleteUser = asyncHandler(async (req, res) => {
+  const { password, email } = req.body;
+
+  const currentUser = await user.findOne({ email });
+
+  let userNotes;
+
+  if (currentUser) {
+    if (await currentUser.comparePwd(password)) {
+      userNotes = await notes.find({ user: currentUser._id });
+
+      if (userNotes) {
+        for (let i = 0; i < userNotes.length; i++) userNotes[i].remove();
+      }
+
+      currentUser.remove();
+
+      res.status(201).json({
+        message: "success",
+      });
+    } else {
+      res.status(400);
+      throw new Error("invalid password");
+    }
+  } else {
+    res.status(400);
+    throw new Error("user not found");
+  }
+});
+
+module.exports = { registerUser, authUser, editUser, deleteUser };
