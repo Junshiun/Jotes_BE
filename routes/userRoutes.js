13f9243463ad570a8b558b4e8a789eaf64a480b0
Junshiun <55488934+Junshiun@users.noreply.github.com>
2022-05-10 17:23:19 +0800

    add user profile edit routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,16 @@
 const express = require("express");
-const { registerUser, authUser } = require("../controllers/userController");
+const {
+  registerUser,
+  authUser,
+  editUser,
+  deleteUser,
+} = require("../controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
 
 const router = express.Router();
 
 router.route("/").post(registerUser);
 router.route("/login").post(authUser);
+router.route("/profile").put(editUser).delete(deleteUser);
 
 module.exports = router;
