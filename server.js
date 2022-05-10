9fad280d8f67c7190486e4005a52e97921426f86
Junshiun <55488934+Junshiun@users.noreply.github.com>
2022-05-10 22:04:04 +0800

    modify port settings

--- server.js
+++ server.js
@@ -53,6 +53,6 @@ app.use("/notes", notesRoutes);
 app.use(notFound);
 app.use(errorHandler);
 
-const PORT = process.env.PORT | 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`currently listen to port ${PORT}`));
